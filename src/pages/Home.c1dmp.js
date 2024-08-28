a8b05c50e6722cdb22f1ead830c258106e9a1791
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2024-08-28 14:40:13 +0000

    Initial commit

---
+++ src/pages/Home.c1dmp.js
@@ -0,0 +1,49 @@
+// Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+
+$w.onReady(function () {
+
+	// Write your Javascript code here using the Velo framework API
+	// Print hello world:
+	// console.log("Hello world!");
+
+	// Call functions on page elements, e.g.:
+	// $w("#button1").label = "Click me!";
+
+	// Click "Run", or Preview your site, to execute your code
+
+});
+
+/**
+*	Adds an event handler that runs when the HTML Component
+ sends a message.
+	[Read more](https://www.wix.com/corvid/reference/$w.HtmlComponent.html#onMessage)
+*	 @param {$w.HtmlComponentMessageEvent} event
+*/
+export function html1_message(event) {
+	// This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+	// Add your code for this event here: 
+}
+
+/**
+*	Adds an event handler that runs when the pointer is moved
+ onto the element.
+
+ You can also [define an event handler using the Properties and Events panel](https://support.wix.com/en/article/velo-reacting-to-user-actions-using-events).
+	[Read more](https://www.wix.com/corvid/reference/$w.Element.html#onMouseIn)
+*	 @param {$w.MouseEvent} event
+*/
+export function html1_mouseIn(event) {
+	// This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+	// Add your code for this event here: 
+}
+
+/**
+*	Adds an event handler that runs when an element is displayed
+ in the viewable part of the current window.
+	[Read more](https://www.wix.com/corvid/reference/$w.ViewportMixin.html#onViewportEnter)
+*	 @param {$w.Event} event
+*/
+export function html1_viewportEnter(event) {
+	// This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+	// Add your code for this event here: 
+}
